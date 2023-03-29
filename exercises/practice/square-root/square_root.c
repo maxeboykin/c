55d9f9d091dd90fa56f7e64f9d1a71408a269936
maxxboykin <maxericboykin@gmail.com>
2023-03-29 13:09:23 -0400

    adding hamming and square root

--- exercises/practice/square-root/square_root.c
+++ exercises/practice/square-root/square_root.c
@@ -1,1 +1,9 @@
 #include "square_root.h"
+
+int square_root(int radicand) {
+   int root = 1;
+   while (root * root != radicand && (root)*(root) < radicand){
+      root += 1;
+   }
+   return root;
+}
